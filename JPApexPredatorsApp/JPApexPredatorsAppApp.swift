4fa117d7ed72bfb72a27b9459ec4ebc8a09e64bd
Mounika Kola <mounika.kola@pse.com>
2026-01-05 17:53:03 +0530

    Initial Commit

---
+++ JPApexPredatorsApp/JPApexPredatorsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsAppApp.swift
+//  JPApexPredatorsApp
+//
+//  Created by m.kola on 05/01/26.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
